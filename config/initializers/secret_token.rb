ac802ae42f4e4e9b9bef0416d082fda4dca325a3
weilongw <weilongw@andrew.cmu.edu>
2012-09-21 17:29:04 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamePort::Application.config.secret_token = '2df1d23d3f3f0986ef434ec17534bd7d669f00018f24394aa88d4b9d69ee332a7b95f78d2b9cd60d92fead7e2232a8dffb3723639a62a58184a3c102ef95e565'
